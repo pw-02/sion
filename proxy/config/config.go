26bce2d8a7bbbb09100f7395948c9cc6284c486d
Patrick Watters <>
2023-02-15 15:20:20 -0800

    C1

--- proxy/config/config.go
+++ proxy/config/config.go
@@ -22,7 +22,7 @@ const ProxyFeatures = FLAG_CLIENTSIDE_FIRSTD_OPTIMIZATION | FLAG_ENABLE_LOCAL_CA
 const LambdaLogLevel = logger.LOG_LEVEL_ALL // Set to logger.LOG_LEVEL_ALL to keep Lambda log level aligned with proxy log level.
 
 // LambdaPrefix Prefix of Lambda function, overridable with command line parameter -lambda-prefix.
-const LambdaPrefix = "Your Lambda Function Prefix"
+const LambdaPrefix = "CacheNode"
 
 // AWSRegion Region of AWS services.
 const AWSRegion = "us-west-1"
