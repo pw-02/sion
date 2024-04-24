2b43f843bf2a5f59963d33591e3ba1251b383b52
pw-02 <watters.patrick@outlook.com>
2024-04-24 11:40:26 -0700

    protocol.FLAG_ENABLE_PERSISTENT disabled

--- proxy/config/config.go
+++ proxy/config/config.go
@@ -18,7 +18,7 @@ const FLAG_CLIENTSIDE_FIRSTD_OPTIMIZATION = 0x0002
 const FLAG_ENABLE_LOCAL_CACHE = 0x0004
 
 // const LambdaFeatures = protocol.FLAG_ENABLE_WARMUP | protocol.FLAG_ENABLE_PERSISTENT | protocol.FLAG_DISABLE_WAIT_FOR_COS
-const LambdaFeatures = protocol.FLAG_ENABLE_WARMUP | protocol.FLAG_ENABLE_PERSISTENT | protocol.FLAG_DISABLE_WAIT_FOR_COS
+const LambdaFeatures = protocol.FLAG_ENABLE_WARMUP | protocol.FLAG_DISABLE_WAIT_FOR_COS
 
 //const LambdaFeatures = protocol.FLAG_DISABLE_WAIT_FOR_COS
 
