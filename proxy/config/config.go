7b14e772e96130d4a377762ca29d0b226eae519d
pw-02 <watters.patrick@outlook.com>
2024-10-11 10:57:47 -0700

    const LambdaMaxDeployments = 1000

--- proxy/config/config.go
+++ proxy/config/config.go
@@ -31,7 +31,7 @@ const LambdaPrefix = "CacheNode"
 const AWSRegion = "us-west-2"
 
 // LambdaMaxDeployments Number of Lambda function deployments available.
-const LambdaMaxDeployments = 400
+const LambdaMaxDeployments = 1000
 
 // Mode of cluster.
 const StaticCluster = "static"
