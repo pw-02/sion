90b6c42d5114fa262221268b7ee7aec9836f1a88
pw-02 <watters.patrick@outlook.com>
2025-02-21 15:28:52 -0800

    t BackupsPerInstance = 20

--- proxy/config/config.go
+++ proxy/config/config.go
@@ -77,7 +77,7 @@ const ServerPublicIp = "" // Leave it empty if Lambda VPC is enabled.
 const RecoverRate = 40 * 1000000 // Not actually used.
 
 // BackupsPerInstance  Number of backup instances used for parallel recovery.
-const BackupsPerInstance = 0 // (InstanceCapacity - InstanceOverhead) / RecoverRate
+const BackupsPerInstance = 20 // (InstanceCapacity - InstanceOverhead) / RecoverRate
 
 // Each bucket's active duration
 const BucketDuration = 10 // min
