717028d98c1d36ea87769ac5afcd53b9488b8444
pw-02 <watters.patrick@outlook.com>
2025-02-21 12:40:41 -0800

    const NumLambdaClusters = 16

--- proxy/config/config.go
+++ proxy/config/config.go
@@ -46,7 +46,7 @@ const SliceSize = 100
 
 // NumLambdaClusters Number of Lambda function deployments initiated on launching.
 // For window cluster, this must be at least D+P
-const NumLambdaClusters = 13
+const NumLambdaClusters = 16
 
 // LambdaStoreName Obsoleted. Name of Lambda function for replica version.
 const LambdaStoreName = "LambdaStore"
